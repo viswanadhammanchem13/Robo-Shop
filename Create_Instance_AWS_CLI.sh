fd1a819058bddf002fa796e6927253b05b5eb9bf
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-05-27 13:49:47 +0530

    Shell Script Started

--- Create_Instance_AWS_CLI.sh
+++ Create_Instance_AWS_CLI.sh
@@ -8,7 +8,7 @@ DOMAIN_NAME="manchem.site"
 
 for instances in ${Instances[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-05d301070925037f6 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name, Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-05d301070925037f6 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name, Value=$instances}]' --query 'Instances[0].InstanceId' --output text)
     if [ $instances != "Frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
