4d0db1600432eead1cc0d1c8e59fec265e4748ba
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-05-27 12:51:13 +0530

    Shell Script Started

---
+++ Create_Instance_AWS_CLI.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-05d301070925037f6"
+Instances=("Mongo_DB" "Catalouge" "Reddis" "Users" "Cart" "MySQL" "Shipping" "Rabbit_MQ" "Payment" "Dispatch" "Frontend")
+ZONE_ID="Z03584735O3LYRT2Q9HU"
+DOMAIN_NAME="manchem.site"
+
+for instances in ${Instances[@]}
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    fi
+    echo "$instance IP address: $IP"
+done
