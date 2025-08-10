638a92063d37f5d08d88a8555d9d0a71ff5b151e
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-10 20:30:25 +0530

    Simple Mathmatics

--- Create_Instance_AWS_CLI.sh
+++ Create_Instance_AWS_CLI.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-05d301070925037f6"
-Instances=("Mongo_DB" "Catalouge" "Reddis" "Users" "Cart" "MySQL" "Shipping" "Rabbit_MQ" "Payment" "Dispatch" "Frontend")
+SG_ID="sg-03e454532f3f29b07"
+Instances=("MongoDB" "Catalouge" "Reddis" "Users" "Cart" "MySQL" "Shipping" "Rabbit_MQ" "Payment" "Dispatch" "Frontend")
 ZONE_ID="Z03584735O3LYRT2Q9HU"
 DOMAIN_NAME="manchem.site"
 
 for instances in ${Instances[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-05d301070925037f6 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name, Value=$instances}]' --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-03e454532f3f29b07 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name, Value=$instances}]' --query 'Instances[0].InstanceId' --output text)
     if [ $instances != "Frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
