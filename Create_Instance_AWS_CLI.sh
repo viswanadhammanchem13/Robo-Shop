ef63d70f38fc4a57af3a1158ac39909f2658f1ca
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-12 06:52:23 +0530

    Simple Mathmatics

--- Create_Instance_AWS_CLI.sh
+++ Create_Instance_AWS_CLI.sh
@@ -15,29 +15,22 @@ do
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
     fi
     echo "$instances IP address: $IP"
-
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \  
-    --change-batch 
-    '{ 
-        "Comment": "Creating or Updating a record set for cognito endpoint", 
-        "Changes": 
-        [ 
-            { 
-                "Action": "UPSERT", 
-                "ResourceRecordSet": 
-                { 
-                    "Name": "'$instance'"."'$DOMAIN_NAME'", 
-                    "Type": "A", 
-                    "TTL": 1, 
-                    "ResourceRecords": 
-                    [ 
-                        { 
-                            "Value": $IP
-                         } 
-                    ] 
-                } 
-            } 
-        ] 
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$instance'"."'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
     }'
+    
 done
