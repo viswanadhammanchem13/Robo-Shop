83538c46ad2178d18049694556ec34659e8b9361
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-13 21:11:49 +0530

    Simple Mathmatics

--- Reddis.sh
+++ Reddis.sh
@@ -31,18 +31,18 @@ Validate (){ #Function Definition
     fi #Condition Ends
 }
 
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 Validate $? "Disabling Redis"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 Validate $? "Enabling Redis"
 
-dnf install redis -y
+dnf install redis -y &>>$LOG_FILE
 Validate $? "Installing Redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
 Validate $? "Edited redis.conf to accept remote connections"
 
-systemctl enable redis 
-systemctl start redis
+systemctl enable redis &>>$LOG_FILE
+systemctl start redis &>>$LOG_FILE
 Validate $? "Starting Reddis Service"
