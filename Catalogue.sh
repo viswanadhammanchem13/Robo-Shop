4a1ffd8f2081f6fcd5351c7a961e7393e7d0999b
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-13 07:51:23 +0530

    Simple Mathmatics

--- Catalogue.sh
+++ Catalogue.sh
@@ -84,6 +84,13 @@ Validate $? "Coping MongoDB Repo"
 dnf install mongodb-mongosh -y
 Validate $? "Installing MongoDB Client"
 
-mongosh --host mongodb.manchem.site  </app/db/master-data.js
+STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue"))
+if [ $STATUS -eq 0 ]
+then
+    echo " Data is Already Loaded"
+else
+    echo "Data is Not Loaded..Loading"
+    mongosh --host mongodb.manchem.site  </app/db/master-data.js
+
 Validate $? "Loading the Data"
 
