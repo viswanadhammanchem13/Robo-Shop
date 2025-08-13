0b4edad40a8585ee252c793b3c27e1a2d6133059
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-13 21:10:28 +0530

    Simple Mathmatics

--- Reddis.sh
+++ Reddis.sh
@@ -41,7 +41,7 @@ dnf install redis -y
 Validate $? "Installing Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Edited redis.conf to accept remote connections"
+Validate $? "Edited redis.conf to accept remote connections"
 
 systemctl enable redis 
 systemctl start redis
