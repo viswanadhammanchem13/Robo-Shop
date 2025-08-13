dd8d6ba0996d9413d570fef32c392ef42128665b
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-13 06:46:43 +0530

    Simple Mathmatics

--- MongoDB.sh
+++ MongoDB.sh
@@ -5,9 +5,11 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_Folder="/var/log/ShellScriptlogs"
+
+LOGS_Folder="/var/log/Roboshop"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_Folder/$SCRIPT_NAME.log"
+mkdir Roboshop
 echo -e  "$R Script Executed at:$TIME $N" | tee -a $LOG_FILE # Tee command Display the content on Screen
 if [ $USERID -ne 0 ] #Checks Whether UID is = 0 or not
 then #!= 0 Enter into Loop
