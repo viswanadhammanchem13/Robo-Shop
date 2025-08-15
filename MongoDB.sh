d0f19f9ac924284911f308ef003a0536a053acfb
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-15 06:14:26 +0530

    Simple Mathmatics

--- MongoDB.sh
+++ MongoDB.sh
@@ -35,16 +35,16 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>>LOG_FILE
 Validate $? "Coping of MongoDB Repo"
 
 dnf install mongodb-org -y &>>LOG_FILE
-Validate $? "MySQL Installation"
+Validate $? "MongoDB Installation"
 
 systemctl enable mongod | tee -a $LOG_FILE
-Validate $? "MySQL Service Enabled" 
+Validate $? "MongoDB Service Enabled" 
 
 systemctl start mongod | tee -a $LOG_FILE
-Validate $? "MySQL Service Started" 
+Validate $? "MongoDB Service Started" 
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 Validate $? "Editing MongoDB conf file for remote connections"
 
 systemctl restart mongod
-Validate $? "MySQL Service ReStarted" 
+Validate $? "MongoDB Service ReStarted" 
