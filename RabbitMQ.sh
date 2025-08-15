90f850b7259fc3e3d36643185cf35d1bda0f4b53
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2025-08-15 12:45:06 +0530

    Simple Mathmatics

--- RabbitMQ.sh
+++ RabbitMQ.sh
@@ -50,8 +50,8 @@ fi
 # cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
 # Validate $? "Copying RabbitMQ Repo"
 
-# dnf install rabbitmq-server -y &>>$LOG_FILE
-dnf install rabbitmq-server -y 2>&1 | tee -a $LOG_FILE
+dnf install erlang rabbitmq-server -y &>>$LOG_FILE
+# dnf install rabbitmq-server -y 2>&1 | tee -a $LOG_FILE
 Validate $? "Installing RabbitMQ Server"
 
 systemctl enable rabbitmq-server &>>$LOG_FILE
